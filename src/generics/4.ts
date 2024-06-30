37c2ebd46b4639cc5a6f8391fce02e9d36e30b0a
Anna Taranchuk <taranchuk4@yahoo.com>
2024-06-30 18:15:00 +0300

    add tasks

---
+++ src/generics/4.ts
@@ -0,0 +1,16 @@
+type UserProfile = {
+  name: string;
+  surname: string;
+  email: string;
+  password: string;
+};
+
+function createOrUpdateUser(initialValues: Partial<UserProfile>) {
+  // Оновлення користувача
+  return { ...initialValues };
+}
+
+createOrUpdateUser({
+  email: 'user@mail.com',
+  password: 'password123',
+});
